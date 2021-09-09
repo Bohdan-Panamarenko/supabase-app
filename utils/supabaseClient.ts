847f9160ca40f6f8d1b9b9440c580c60a24c375c
Bohdan Panamarenko <panamarenkobohdan@gmail.com>
2021-09-09 19:02:52 +0000

    Supabase access data

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = <string>process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = <string>process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://digithgdhukwunpiejxj.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDk5ODU5OCwiZXhwIjoxOTQ2NTc0NTk4fQ.rQeTcVZItSpBwvnTxLgYsd_Odp_hG62_45XJXsdaZlY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
